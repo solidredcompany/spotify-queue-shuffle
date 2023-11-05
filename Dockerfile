1e0ba4c617ab60172300869e35a2873379c4dfa3
Hannes Hultergård <hannes.hultergard@gmail.com>
2023-11-05 11:55:55 +0100

    Fix docker and deployment

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /app
 COPY go.mod ./
 
 COPY *.go ./
+COPY internal ./internal
 COPY web ./web
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o /queue-shuffle
--- build-and-load.sh
+++ build-and-load.sh
@@ -5,3 +5,5 @@ docker image rm spotify-queue-shuffle
 scp spotify-queue-shuffle.tar root@10.150.0.4:
 
 ssh root@10.150.0.4 'docker load -i spotify-queue-shuffle.tar'
+
+rm spotify-queue-shuffle.tar
